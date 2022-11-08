3689effaba2a4550ebfc955a16fcd7d9f7b1be13
Patryk Jełowicki <patryk.jelowicki@gmail.com>
2022-11-08 16:11:56 +0000

    Lesson 6 homework

---
+++ src/main/java/lesson5/NumericVariables.java
@@ -0,0 +1,13 @@
+package lesson5;
+
+public class NumericVariables {
+
+
+    public static void main(String[] args) {
+
+        int wynik;
+        System.out.println("dodawanie");
+        wynik =  20 + 20;
+        System.out.println(wynik);
+    }
+}
